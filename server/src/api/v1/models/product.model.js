5a596ec1357638fc6bfdb186210361db35032053
dhtuan <hatuan1423@gmail.com>
2025-02-01 18:29:33 +0700

    model: product

---
+++ server/src/api/v1/models/product.model.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const { model, Schema } = require("mongoose")
+const DOCUMENT_NAME = 'Product'
+const COLLECTION_NAME = 'Products'
+
+const productSchema = new Schema({
+    product_name: { type: String, required: true },
+    product_thumb: { type: String, required: true },
+    product_description: { type: String, required: true },
+    product_price: { type: Number, required: true },
+    product_quantity: { type: Number, required: true },
+    product_type: { type: String, required: true, enum: ['Electronic', 'Clothing', 'Furniture'] },
+    product_shop: { type: Schema.Types.ObjectId, ref: 'Shop' },
+    product_attributes: { type: Schema.Types.Mixed, required: true }
+},
+    {
+        timestamps: true,
+        collection: COLLECTION_NAME
+    })
+
+const clothingSchema = new Schema({
+    brand: { type: String, required: true },
+    size: { type: String },
+    material: { type: String }
+}, {
+    collection: 'Clothes',
+    timestamps: true
+})
+
+module.exports = {
+    product: model(DOCUMENT_NAME, productSchema),
+    clothing: model('Clothing', clothingSchema)
+}
